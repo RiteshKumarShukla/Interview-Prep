b49db20736f37c7d41fd1d7210eed1a36ce185d7
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-06-24 10:13:21 +0530

    Create SearchBar.jsx

---
+++ SearchBar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.example.com/search?q=${searchTerm}`
+      );
+      const data = await response.json();
+      setSearchResults(data.results);
+    } catch (error) {
+      console.error("Error performing search:", error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.id}>{result.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Search;
+
+
+
+
+
+
+
+
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+  
+  Using Filter:
+
+
+import React, { useState } from "react";
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const data = [
+    { id: 1, title: "Result 1" },
+    { id: 2, title: "Result 2" },
+    { id: 3, title: "Result 3" },
+    { id: 4, title: "Result 4" },
+    { id: 5, title: "Result 5" },
+  ];
+
+  const handleSearch = () => {
+    const filteredResults = data.filter((result) =>
+      result.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(filteredResults);
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <ul>
+        {searchResults.map((result) => (
+          <li key={result.id}>{result.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Search;
