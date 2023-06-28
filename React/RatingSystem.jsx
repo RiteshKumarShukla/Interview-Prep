6c59e18aabc84089c4f1d6c88610176c3d0ce72a
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-06-28 13:24:24 +0530

    rating system

---
+++ React/RatingSystem.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+const RatingSystem = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+  };
+
+  return (
+    <div>
+      <h2>Rating: {rating}</h2>
+      <div>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <Star key={value} value={value} onClick={handleRatingChange} filled={value <= rating} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Star = ({ value, onClick, filled }) => {
+  const starStyles = {
+    cursor: 'pointer',
+    color: filled ? 'gold' : 'gray',
+    fontSize: '100px', // Increase the font size for larger stars
+  };
+
+  return <span style={starStyles} onClick={() => onClick(value)}>*</span>;
+};
+
+export default RatingSystem;
