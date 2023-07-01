8f0df44d049d51206c6e4908a05ca68e5da4ee04
Ritesh Kumar Shukla <riteshshuklagem@gmail.com>
2023-07-01 13:25:56 +0530

    table component

--- React/tableComp.jsx
+++ React/tableComp.jsx
@@ -1,45 +1,53 @@
 import React, { useEffect, useState } from 'react';
 
-function DataFetchComponent() {
+const DataFetchingComponent = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (response.ok) {
+          const jsonData = await response.json();
+          setData(jsonData);
+        } else {
+          console.error('Error fetching data:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
     fetchData();
   }, []);
 
-  async function fetchData() {
-    try {
-      const response = await fetch('https://api.example.com/data');
-      const jsonData = await response.json();
-      setData(jsonData);
-    } catch (error) {
-      console.log('Error fetching data:', error);
-    }
-  }
-
   return (
     <div>
-      <h1>Data Table</h1>
+      <h1>Data Fetching Example</h1>
       <table>
         <thead>
           <tr>
             <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th>Description</th>
           </tr>
         </thead>
         <tbody>
           {data.map((item) => (
             <tr key={item.id}>
               <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
+              <td>{item.title}</td>
+              <td>{item.price}</td>
+              <td>{item.category}</td>
+              <td>{item.description}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
-export default DataFetchComponent;
+export default DataFetchingComponent;
